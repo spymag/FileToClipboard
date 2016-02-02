234d9bd1d72cc9451ab9d633987c1cdde471743b
Spyros Magripis <mechoulas@gmail.com>
2016-02-02 18:36:44 +0100

    Update FileToClipboard.py

--- FileToClipboard.py
+++ FileToClipboard.py
@@ -6,14 +6,38 @@ import os
 
 
 #definitions for debuging only
-fname = "FileToParse.txt"
+fname = "BFV4_CombinedHoisting_instance1_35.97T@93.281m.txt"
+fname1 = "BFV4_CombinedHoisting_instance1_80.545T@85.40m.txt"
+fname2 = "BFV4_CombinedHoisting_instance1_170.13T@72m.txt"
+fname3 = "BFV4_CombinedHoisting_instance1_450.91T@41.18m.txt"
+fname4 = "BFV4_CombinedHoisting_instance1_174.17T@68.49m.txt"
+fname5 = "BFV4_CombinedHoisting_instance1_210.3T@75m.txt"
+fname6 = ""
+
+outreach = 93.21
+outreach1 = 85.40
+outreach2 = 72.00
+outreach3 = 41.18
+outreach4 = 68.49
+outreach5 = 75.00
+outreach6 = 0
+
 ofile = "Output.txt"
-outreach = 15.1
 filtered = []
 noSpaces = []
 inputs = []
+filesNumber = [fname,fname1,fname2,fname3,fname4,fname5,fname6]
+outreachList = [outreach,outreach1,outreach2,outreach3,outreach4,outreach5,outreach6]
+flag = 0
+lengths = []
+noBlanks = []
+joinedList = []
+noDuplicates = []
 
-
+def f7(seq):
+    seen = set()
+    seen_add = seen.add
+    return [x for x in seq if not (x in seen or seen_add(x))]
 
 # read all the lines from a file and add them to a list
 def ReadFileLines(fname):
@@ -21,32 +45,75 @@ def ReadFileLines(fname):
         content = f.readlines()
     return content
 
-#write all the strings to the list
-inputs = ReadFileLines(fname)
+#print(len(filesNumber))
+#print(len(ReadFileLines(filesNumber[1])))
 
+
+
+#create a list with the lengths of each file filtering out the blank lines
+for i in range(0,len(filesNumber)):
+    if filesNumber[i] != "":
+        temp = ReadFileLines(filesNumber[i])
+        #print(temp)
+        lengthunit = 0
+        for i in range(0,len(temp)):           
+            if temp[i] != "\n":
+                lengthunit =lengthunit + 1
+        lengths.append(lengthunit)
+        #print(ReadFileLines(filesNumber[i]))
+    
+print(lengths)    
+
+#print len(ReadFileLines(filesNumber[0]))
+
+#write all the strings to the list from all the files
+for i in range(0,len(filesNumber)):
+    if filesNumber[i] != "":
+        #inputs = ReadFileLines(filesNumber[i])
+        inputs.extend(ReadFileLines(filesNumber[i]))
+
+#print(inputs)
 #remove spaces from the list
 for i in range(0,len(inputs)):
     noSpaces.append(inputs[i].split())
-#print(noSpaces[1][0][0:3])
+#print(noSpaces)
+#print ((lengths[0])+12)
 
+#remove empty strings from the list
+#noBlanks = filter(None, noSpaces) # no support in 3.4.4??
+for i in range(0,len(noSpaces)):
+    if noSpaces[i]!= []:
+        noBlanks.append(noSpaces[i])
+    
+#print(noBlanks)
 
 #append lines into the filtered list
-for i in range(0,len(noSpaces)):
+for j in range(0,len(lengths)): # first select the outreach of the each file,
+    i=0
+    for i in range(i+flag,lengths[j]+flag): # then iterate through all the list items
 
-    #if the first figure of the line is equal to the selected outreach...
-    if noSpaces[i][0][0:(len(str(outreach)))] == str(outreach):
-        #print(noSpaces[i])
-        filtered.append(noSpaces[i])
-#print(len(filtered[0]))
+        #if the first figure of the line is equal to the selected outreach...
+        if noBlanks[i][0][0:(len(str(outreachList[j])))] == str(outreachList[j]):
+            #print(noBlanks[i])
+            filtered.append(noBlanks[i])
+    flag = flag + lengths[j]
+#print(len(ReadFileLines(filesNumber)[0]))
+
+#print(filtered)
+#join the list
+for i in range(0,len(filtered)):
+    joinedList.append(" ".join(filtered[i]))
+
+#remove duplicates
+noDuplicates = f7(joinedList)    
+#print(noDuplicates)
 
 #print ReadFileLines(fname)
 with open(ofile, "w") as text_file:
-    for i in range(0,len(filtered)):
-        for j in range(0,len(filtered[i])):
-            text_file.write(filtered[i][j])
-            text_file.write(' ')# add space for each list item
+    for i in range(0,len(noDuplicates)):
+        text_file.write(noDuplicates[i])
         text_file.write('\n') # add new line every after line
         locations = os.getcwd() +'/'+ str(ofile)
         
-print(" %s matches found! and copied to %s" % (str(len(filtered)), locations))
+print(" %s matches found! and copied to %s" % (str(len(noDuplicates)), locations))
 text_file.close()
